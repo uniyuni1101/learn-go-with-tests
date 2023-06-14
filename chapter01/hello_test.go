38211b2b77c2e0d6bc2acaaef36a396b9164d858
uniyuni1101 <uniyuni1101@gmail.com>
2023-06-14 22:06:43 +0900

    modify insert default string if argument is empty

--- chapter01/hello_test.go
+++ chapter01/hello_test.go
@@ -3,12 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-  got := Hello("Chris")
-  want := "Hello, Chris"
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
 
-  if got != want {
-    t.Errorf("got %q want %q", got, want)
-  }
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
-
-
