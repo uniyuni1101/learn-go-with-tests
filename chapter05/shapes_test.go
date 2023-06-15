3eb9a522cd40d44dd620097b5c92d1dc4b99590a
uniyuni1101 <uniyuni1101@gmail.com>
2023-06-15 21:58:26 +0900

    Practice Chapter05 struct, method, interface

---
+++ chapter05/shapes_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func TestArea(t *testing.T) {
+
+	areaTestCases := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tc := range areaTestCases {
+		got := tc.shape.Area()
+		if got != tc.want {
+			t.Errorf("%#v got %.2f want %.2f", tc.shape, got, tc.want)
+		}
+	}
+}
