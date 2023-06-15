5a9669e8d1ff1815e39d682657de70815b36228a
uniyuni1101 <uniyuni1101@gmail.com>
2023-06-15 18:37:22 +0900

    Solve Chapter3 practice exercise

--- chapter03/repeart.go
+++ chapter03/repeart.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeart(charactor string) string {
+func Repeart(charactor string, repeart int) string {
 	var repearted string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeart; i++ {
 		repearted += charactor
 	}
 	return repearted
--- chapter03/repeart_test.go
+++ chapter03/repeart_test.go
@@ -1,19 +1,44 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestRepeart(t *testing.T) {
-	repearted := Repeart("a")
-	expected := "aaaaa"
-
+func repeartHelper(t testing.TB, char string, repeart int, expected string) {
+	t.Helper()
+	repearted := Repeart(char, repeart)
 	if repearted != expected {
 		t.Errorf("expected %q but got %q", expected, repearted)
 	}
 }
 
+func TestRepeart(t *testing.T) {
+	t.Run("5 repeart", func(t *testing.T) {
+		char := "a"
+		repeart := 5
+		expected := "aaaaa"
+
+		repeartHelper(t, char, repeart, expected)
+	})
+
+	t.Run("10 repeart", func(t *testing.T) {
+		char := "a"
+		repeart := 10
+		expected := "aaaaaaaaaa"
+
+		repeartHelper(t, char, repeart, expected)
+	})
+}
 
 func BenchmarkRepeart(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeart("a")
+		Repeart("a", 5)
 	}
+}
+
+func ExampleRepeart() {
+	repearted := Repeart("a", 5)
+	fmt.Println(repearted)
+	// Output: aaaaa
 }
