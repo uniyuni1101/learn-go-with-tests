bba2481ea6a076569234eeb4e916d958aaf9a60c
uniyuni1101 <uniyuni1101@gmail.com>
2023-06-22 18:07:26 +0900

    Practice chapter13 sync

---
+++ chapter13/sync.go
@@ -0,0 +1,22 @@
+package chapter13
+
+import "sync"
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
